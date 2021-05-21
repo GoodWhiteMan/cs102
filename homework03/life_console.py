15ed65e24c414935070747d21ed2ff6082da40bc
GoodWhiteMan <Krish19poroh@mail.ru>
2021-05-21 20:17:54 +0300

    Реализованы методы класса Console(UI)

--- homework03/life_console.py
+++ homework03/life_console.py
@@ -10,13 +10,47 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        screen.clear()
+
+        height, width = screen.getmaxyx()
+        line = ""
+
+        for row in range(height):
+            for col in range(width):
+                if row == 0 or row == height - 1:
+                    if col == 0 or col == width - 1:
+                        line += "+"
+                    else:
+                        line += "-"
+                elif row > 0 & row < (height - 1):
+                    if col == 0 or col == width - 1:
+                        line += "|"
+                    else:
+                        line += " "
+            try:
+                screen.addstr(line)
+            except curses.error:
+                pass
+            line = ""
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        height, width = screen.getmaxyx()
+        x = (width - self.life.cols) // 2
+        y = (height - self.life.rows) // 2
+
+        for row_num, row in enumerate(self.life.curr_generation):
+            for col_num, col in enumerate(row):
+                if col:
+                    try:
+                        screen.addstr(row_num + y, col_num + x, "*")
+                    except curses.error:
+                        pass
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        curses.wrapper(self.draw_borders)
+        while self.life.is_changing and not self.life.is_max_generations_exceeded:
+            self.life.step()
+            self.draw_borders(screen)
         curses.endwin()
